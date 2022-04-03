51e056698a6d28733e57ca7a0189e64b9f5c79d2
fereshte <fereshte.bahramy@gmail.com>
2022-04-04 00:02:14 +0430

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/fereshtebahrami"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Fereshte Bahrami
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/FereshteBahrami/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/fereshtebahrami"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Fereshte Bahrami
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/FereshteBahrami/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
