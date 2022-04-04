d09e666e11ee7c85447bdfbd15efc2db283f41dd
fereshte <fereshte.bahramy@gmail.com>
2022-04-04 16:44:09 +0430

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,7 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./Weather.css";
+
 export default function Weather() {
   return (
     <div className="Weather">
@@ -10,10 +12,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -21,15 +28,20 @@ export default function Weather() {
       <h1>New York</h1>
       <ul>
         <li>Wednesday 07:00</li>
-        <li>mostly cloudy</li>
+        <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          6°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt="Mostly Cloudy"
+            />
+            <div classNmae="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
