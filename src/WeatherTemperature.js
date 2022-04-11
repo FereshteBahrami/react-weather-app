61e6ea6a0c90ee908f4de5a4d37d8a5556937dad
fereshte <fereshte.bahramy@gmail.com>
2022-04-11 17:01:39 +0430

    Refresh forecast and simplify units

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,10 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsuis");
-
-  function showFarenheit(event) {
-    event.preventDefault();
-    setUnit("farenheit");
-  }
-
-  function showcelsuis(event) {
-    event.preventDefault();
-    setUnit("celsuis");
-  }
-  function farenheit() {
-    return (props.celsuis * 9) / 5 + 32;
-  }
-
-  if (unit === "celsuis") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsuis)}</span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={showFarenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(farenheit())}</span>
-        <span className="unit">
-          <a href="/" onClick={showcelsuis}>
-            {" "}
-            °C{" "}
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsuis)}</span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
